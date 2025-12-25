af1068ef79359b57e2080953356804732f8b49eb
tess647 <thanni_taslihah@yahoo.com>
2025-12-25 07:19:21 +0100

    deployment

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const connectDB = async () => {
 
 connectDB();
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(` App running on port ${port}...`);
 });
